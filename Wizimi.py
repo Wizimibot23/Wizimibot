037bb301acf3a49fea1bf8d4cf0d41242744399c
Wizimibot23 <wizimk28@gmail.com>
2025-07-07 01:05:09 +0100

    Wizimi.py
    Signed-off-by: Wizimibot23 <wizimk28@gmail.com>

---
+++ Wizimi.py
@@ -0,0 +1,9 @@
+git init
+git add .
+git commit -m "🚀 Premier commit : Wizimibot"
+git branch -M main
+git remote add origin https://github.com/Wizimibot23/wizimibot.git
+git push -u origin mainhttps://github.com/Wizimibot23/wizimibot.gitstart.shchmod +x start.shrequirements.txt#!/usr/bin/env bash
+pip install -r requirements.txt
+python wizimibot.pypython-telegram-bot==20.3wizimibot.pymkdir wizimibot
+cd wizimibot
